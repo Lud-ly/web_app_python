56b2bf8be18a249298f6d47df165be7916bac0c9
Ludovic Mouly <ludom82@gmail.com>
2024-10-04 19:11:13 +0200

    creating an image cropper with rembg

--- removebg.py
+++ removebg.py
@@ -1,2 +1,9 @@
 # Script python pour streamlit pour une interface pour détourer des images avec la lib rembg
 import streamlit as st
+
+st.title("Mon détoureur d'images")
+uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+if uploaded_file is not None:
+    st.download_button("Télécharger", "newImage.png",uploaded_file, mime="image/png")
+else:
+    st.write("Upload an image")
