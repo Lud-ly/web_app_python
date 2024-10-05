d707571acb795d24c9eacd5d1932ab8c07dfc590
Ludovic Mouly <ludom82@gmail.com>
2024-10-05 11:25:42 +0200

    rembg test

--- removebg.py
+++ removebg.py
@@ -3,15 +3,19 @@ import streamlit as st  # Framework pour créer des applications web interactive
 from io import BytesIO  # Permet de gérer les flux de données en mémoire (utile pour l'enregistrement d'images)
 from rembg import remove  # Bibliothèque pour supprimer l'arrière-plan des images
 
+default_image = Image.open("./images/goat.jpg") 
+default_deimage = Image.open("./images/dgoat.png")
+
 st.set_page_config(page_title="Détoureur d'images", page_icon="skull", layout="centered")  # Configuration de la page
 
-# Titre de l'application
+# Titre et logo de l'application
+st.sidebar.image("./images/logo.png", width=250)
 st.sidebar.title("Mon détoureur d'images")
 st.write("Ce détoureur d'images est conçu pour supprimer l'arrière-plan d'une image.")
 
 col1, col2 = st.columns(2)
 
-st.write("**Note:** Ce détoureur fonctionne avec les formats PNG, JPG et JPEG.")
+st.write("**Note:** Ce détoureur fonctionne avec les formats PNG, JPG, WEBP et JPEG.")
 
 # Fonction pour convertir l'image traitée en un format téléchargeable
 def convert_image_to_downloadable(image):
@@ -34,17 +38,15 @@ def fix_image(image):
     st.sidebar.download_button("Télécharger l'image", convert_image_to_downloadable(fixed), 'image_detouree.png', 'image/png')
 
 # Chargement du fichier téléversé
-image_upload = st.sidebar.file_uploader("Glissez-déposez un fichier ici", type=["png", "jpg", "jpeg"])
+image_upload = st.sidebar.file_uploader("Glissez-déposez un fichier ici", type=["png", "jpg", "jpeg","webp"])
 
 # Loader pour indiquer le traitement
-with st.spinner('Traitement de l\'image en cours...'):
-    if image_upload:
+if image_upload:
+    with st.spinner('Traitement de l\'image en cours...'):
         image = Image.open(image_upload)  # Ouvre l'image téléversée
         fix_image(image)  # Passe l'image téléversée à la fonction de traitement
-    else:
-        image = Image.open("./images/goat.jpg") 
-        default_image = Image.open("./images/dgoat.png")
-        col1.write("Image originale")
-        col1.image(image)
-        col2.write("Image détourée")
-        col2.image(default_image)
+else:
+    col1.write("Image originale")
+    col1.image(default_image)
+    col2.write("Image détourée")
+    col2.image(default_deimage)
